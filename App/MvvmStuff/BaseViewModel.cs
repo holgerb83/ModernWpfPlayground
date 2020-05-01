26094d25b4da82950906a8ebb403b8df2644909d
Holger Börchers <holger@boerchers.org>
2020-05-01 21:41:05 +0200

    updated to latest preview

--- App/MvvmStuff/BaseViewModel.cs
+++ App/MvvmStuff/BaseViewModel.cs
@@ -14,10 +14,10 @@ namespace ModernWpfPlayground.MvvmStuff
             ObjectAccessor = ObjectAccessor.Create(this);
         }
 
-        private readonly Dictionary<string, object> _values = new Dictionary<string, object>();
+        private readonly Dictionary<string, object?> _values = new Dictionary<string, object?>();
         protected readonly ObjectAccessor ObjectAccessor;
 
-        protected IReadOnlyDictionary<string, object> Values => _values;
+        protected IReadOnlyDictionary<string, object?> Values => _values;
 
         protected bool SetProperty<T>(T value, Action<T>? onChanged = null,
             [CallerMemberName] string? propertyName = null)
@@ -25,7 +25,7 @@ namespace ModernWpfPlayground.MvvmStuff
             if (propertyName == null) throw new ArgumentNullException(nameof(propertyName));
             if (_values.TryGetValue(propertyName, out var obj) && Equals(value, obj)) return false;
 
-            _values[propertyName] = value!;
+            _values[propertyName] = value;
             RaisePropertyChanged(propertyName);
             onChanged?.Invoke(value);
             return true;
@@ -34,7 +34,7 @@ namespace ModernWpfPlayground.MvvmStuff
         protected T GetProperty<T>(T defaultValue = default, [CallerMemberName] string? propertyName = null)
         {
             if (propertyName == null) throw new ArgumentNullException(nameof(propertyName));
-            return Values.TryGetValue(propertyName, out var obj) ? (T)obj : defaultValue;
+            return Values.TryGetValue(propertyName, out var obj) ? (T)obj! : defaultValue;
         }
 
         protected void ResetViewModel(Func<string, bool>? predicate = null)
